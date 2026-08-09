b22d8c138a6274937d841203812dfe92f01c2871
Mohammad Akbari <mohammad.akbari@outlook.com>
2026-08-09 21:27:59 +0330

    implemented ShouldQueueAfterCommit to prevent race condition

--- src/Listeners/HandleSheetsDiscovered.php
+++ src/Listeners/HandleSheetsDiscovered.php
@@ -5,19 +5,21 @@ namespace Akbarjimi\ExcelImporter\Listeners;
 use Akbarjimi\ExcelImporter\Events\SheetDiscovered;
 use Akbarjimi\ExcelImporter\Events\SheetsDiscovered;
 use Akbarjimi\ExcelImporter\Repositories\ExcelSheetRepository;
+use Illuminate\Contracts\Queue\ShouldQueueAfterCommit;
+use Illuminate\Queue\InteractsWithQueue;
 
-final readonly class HandleSheetsDiscovered
+final class HandleSheetsDiscovered implements ShouldQueueAfterCommit
 {
+    use InteractsWithQueue;
+
     public function __construct(
-        private ExcelSheetRepository $sheetRepo,
+        private readonly ExcelSheetRepository $sheetRepo,
     ) {}
 
     public function handle(SheetsDiscovered $event): void
     {
-        $sheets = $this->sheetRepo->getByFileId($event->fileId);
-
-        foreach ($sheets as $sheet) {
-            event(new SheetDiscovered($sheet));
+        foreach ($this->sheetRepo->getByFileId($event->fileId) as $sheet) {
+            event(new SheetDiscovered($sheet->id));
         }
     }
 }
