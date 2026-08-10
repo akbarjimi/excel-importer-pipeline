c247a8f2d7f1bf5adfb52a06304df923b2f1f6a1
Mohammad Akbari <mohammad.akbari@outlook.com>
2026-08-10 20:12:07 +0330

    Fixed bugs

--- src/Services/TransformService.php
+++ src/Services/TransformService.php
@@ -7,25 +7,15 @@ use Illuminate\Support\Facades\Config;
 
 final class TransformService
 {
-    private array $transformers = [];
-
-    /**
-     * Load transformer callbacks for a specific sheet
-     */
-    public function load(ExcelSheet $sheet): void
-    {
-        $config = Config::get('excel-importer-sheets.'.$sheet->name.'.transformers', []);
-        $this->transformers = $config;
-    }
-
     /**
      * Apply transformers to a row's content
      */
-    public function apply(array $row): array
+    public function apply(array $row, ExcelSheet $sheet): array
     {
+        $transformers = Config::get('excel-importer-sheets.' . $sheet->name . '.transformers', []);
         foreach ($row as $column => $value) {
-            if (isset($this->transformers[$column])) {
-                $row[$column] = call_user_func($this->transformers[$column], $value);
+            if (isset($transformers[$column])) {
+                $row[$column] = call_user_func($transformers[$column], $value);
             }
         }
 
