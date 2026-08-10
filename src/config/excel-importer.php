2743baa0a5250ecb81121970f70bf6ee0253fcae
Mohammad Akbari <mohammad.akbari@outlook.com>
2026-08-10 20:07:41 +0330

    Added new keys to configurations

--- src/config/excel-importer.php
+++ src/config/excel-importer.php
@@ -7,4 +7,11 @@ return [
     'hash_algo' => env('EXCEL_IMPORTER_HASH_ALGO', 'md5'),
     'queue' => env('EXCEL_IMPORTER_QUEUE', 'default'),
     'mapper_batch_size' => env('EXCEL_IMPORTER_MAPPER_BATCH_SIZE', 500),
+    'max_sheets' => 50,
+    'logging' => [
+        'enabled' => true,
+        'channels' => ['stack'],
+        'level' => 'info', // debug, info, warning, error
+    ],
+    'strict_validation' => false,
 ];
