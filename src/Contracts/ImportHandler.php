82c7de9cfbaf1e6a669011728f648f2a967a2950
Mohammad Akbari <mohammad.akbari@outlook.com>
2026-08-29 13:01:25 +0330

    feat(handler): Add ImportHandler contract with handle(int, iterable): void

--- src/Contracts/ImportHandler.php
+++ src/Contracts/ImportHandler.php
@@ -1,8 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Akbarjimi\ExcelImporter\Contracts;
+
+use Akbarjimi\ExcelImporter\DTOs\ValidatedRow;
 
 interface ImportHandler
 {
+    /**
+     * @param int $fileId The ID of the imported file.
+     * @param iterable<ValidatedRow> $rows Stream of validated rows.
+     */
     public function handle(int $fileId, iterable $rows): void;
 }
