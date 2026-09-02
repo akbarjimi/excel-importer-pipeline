c7feed80a86e9ea7e207fba76feb5365717f4993
Mohammad Akbari <mohammad.akbari@outlook.com>
2026-09-02 12:41:14 +0330

    chore(config): remove unused mapper_batch_size and set secure default hash

--- src/config/excel-importer.php
+++ src/config/excel-importer.php
@@ -4,19 +4,18 @@ return [
     'chunk_size' => env('EXCEL_IMPORTER_CHUNK_SIZE', 1000),
     'insert_batch_size' => env('EXCEL_IMPORTER_INSERT_BATCH_SIZE', 100),
     'default_disk' => env('EXCEL_IMPORTER_DISK', 'local'),
-    'hash_algo' => env('EXCEL_IMPORTER_HASH_ALGO', 'md5'),
+    'hash_algo' => env('EXCEL_IMPORTER_HASH_ALGO', 'sha256'),
     'queue' => env('EXCEL_IMPORTER_QUEUE', 'default'),
-    'mapper_batch_size' => env('EXCEL_IMPORTER_MAPPER_BATCH_SIZE', 500),
     'max_sheets' => 50,
     'logging' => [
         'enabled' => true,
         'channels' => ['stack'],
-        'level' => 'info', // debug, info, warning, error
+        'level' => 'info',
     ],
     'strict_validation' => false,
     'driver' => env('EXCEL_IMPORTER_DRIVER', 'maatwebsite'),
     'drivers' => [
         'maatwebsite' => \Akbarjimi\ExcelImporter\Drivers\MaatwebsiteDriver::class,
-        'openspout'   => \Akbarjimi\ExcelImporter\Drivers\OpenSpoutDriver::class,
-    ],    
+        'openspout' => \Akbarjimi\ExcelImporter\Drivers\OpenSpoutDriver::class,
+    ],
 ];
