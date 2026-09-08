b1e20662fedaad564a96ae00d6388b268ae378ac
Mohammad Akbari <mohammad.akbari@outlook.com>
2026-09-08 13:19:58 +0330

    docs(driver): enforce list<SheetInfo> return type in ExcelReaderDriver::listSheets

--- src/Contracts/ExcelReaderDriver.php
+++ src/Contracts/ExcelReaderDriver.php
@@ -2,10 +2,15 @@
 
 namespace Akbarjimi\ExcelImporter\Contracts;
 
+use Akbarjimi\ExcelImporter\DTOs\SheetInfo;
+
 interface ExcelReaderDriver
 {
     public function readRows(string $filePath, int $sheetIndex, callable $callback): void;
     // callback signature: fn(RowData $row): void
 
+    /**
+     * @return list<SheetInfo>
+     */
     public function listSheets(string $filePath): array;
 }
