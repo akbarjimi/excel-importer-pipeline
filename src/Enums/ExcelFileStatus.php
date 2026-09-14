6de90601639187828de0a365007bc32f61707f24
Mohammad Akbari <mohammad.akbari@outlook.com>
2026-09-14 11:47:03 +0330

    fix(enum): allow READING to ROWS_EXTRACTED transition on ExcelFileStatus

--- src/Enums/ExcelFileStatus.php
+++ src/Enums/ExcelFileStatus.php
@@ -18,7 +18,7 @@ enum ExcelFileStatus: string
     {
         return match ($this) {
             self::PENDING => in_array($new, [self::READING, self::FAILED]),
-            self::READING => in_array($new, [self::ROWS_EXTRACTING, self::FAILED]),
+            self::READING => in_array($new, [self::ROWS_EXTRACTING, self::ROWS_EXTRACTED, self::FAILED]),
             self::ROWS_EXTRACTING => in_array($new, [self::ROWS_EXTRACTED, self::FAILED]),
             self::ROWS_EXTRACTED => in_array($new, [self::PROCESSING, self::FAILED]),
             self::PROCESSING => in_array($new, [self::COMPLETED, self::FAILED]),
