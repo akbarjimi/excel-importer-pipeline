83f42736b1a8141bdee689bb7483a5f259311a9f
Mohammad Akbari <mohammad.akbari@outlook.com>
2026-09-15 13:19:55 +0330

    fix(model): import HasMany for ExcelRow::errors relation

--- src/Models/ExcelRow.php
+++ src/Models/ExcelRow.php
@@ -9,6 +9,7 @@ use Akbarjimi\ExcelImporter\Enums\ExcelRowStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 final class ExcelRow extends Model
